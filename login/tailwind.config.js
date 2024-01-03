e0c61f8e89510e8f2c275cc0e00fde569da99c0a
lLuan Portugal <luanportugal.profissional.tec@gmail.com>
2024-01-03 19:57:22 -0300

    feat: adding file config the tailwind

---
+++ login/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaria: "#E1E9F1",
+        secudaria: "#2563EC",
+      }
+    },
+  },
+  plugins: [],
+}
+
